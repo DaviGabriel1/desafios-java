7e41d993a3640a99b041fb65d1bfabf348074234
Davi Gabriel <davig41441503@gmail.com>
2024-03-01 12:28:08 -0300

    exercicios 1 e 2 concluidos

--- src/poo/exercicio1/Main.java
+++ src/poo/exercicio1/Main.java
@@ -1,7 +1,7 @@
 package poo.exercicio1;
 
 public class Main {
-    public static void main(String args[]){
+    public static void main(String[] args){
         System.out.println(Calculadora.soma(2,2)+"\n"+Calculadora.subtracao(2,2)+"\n"
                 +Calculadora.divisao(2,2)+"\n"+Calculadora.multiplicacao(2,2));
     }
